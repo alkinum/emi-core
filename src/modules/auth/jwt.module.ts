a23db9872c3311b9aa80502b1c9596d4a3724d3c
BackRunner <dev@backrunner.top>
2023-11-16 01:48:57 +0800

    feat: export jwt auth as a module

---
+++ src/modules/auth/jwt.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [],
+  providers: [JwtStrategy],
+  exports: [JwtStrategy],
+})
+export class JwtAuthModule {}
